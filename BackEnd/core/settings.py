a7ff919db82a8a31aa7eb94f0a55e4199d9bd131
Marcos6192005 <marcosvasquez619@gmail.com>
2025-07-07 16:17:10 -0600

    intento_3

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'django_db',
-#        'USER': 'root',
-#        'PASSWORD': 'M,A,,J,H,123',
-#        'HOST': '192.168.110.209',
-#        'PORT': '3306',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'M,A,J,H,123',
+        'HOST': '192.168.110.209',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
